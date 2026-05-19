b8ca7590ab7bd99fa171c618dd987a4e9e6a53a3
bangkhanhkdj09-star <bangkhanhkdj09@gmail.com>
2026-05-19 22:18:10 +0700

    fit

--- VCamJoy/Sources/AppDelegate.m
+++ VCamJoy/Sources/AppDelegate.m
@@ -2,16 +2,27 @@
 #import "MainViewController.h"
 
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    UINavigationController *nav = [[UINavigationController alloc]
-        initWithRootViewController:[[MainViewController alloc] init]];
-    nav.navigationBar.barStyle    = UIBarStyleBlack;
-    nav.navigationBar.translucent = NO;
-    nav.navigationBar.tintColor   = [UIColor colorWithRed:0.55 green:0.36 blue:0.98 alpha:1];
+    self.window.backgroundColor = [UIColor blackColor];
+    
+    MainViewController *main = [[MainViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:main];
+    nav.navigationBarHidden = YES;
+    nav.navigationBar.barStyle = UIBarStyleBlack;
+    
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
+
+- (void)applicationWillResignActive:(UIApplication *)application {}
+- (void)applicationDidEnterBackground:(UIApplication *)application {}
+- (void)applicationWillEnterForeground:(UIApplication *)application {}
+- (void)applicationDidBecomeActive:(UIApplication *)application {}
+
 @end
