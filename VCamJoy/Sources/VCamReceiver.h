933644288dda6be00a83585af00bd0d567b9a240
bangkhanhkdj09-star <bangkhanhkdj09@gmail.com>
2026-05-20 00:12:54 +0700

    ok

--- VCamJoy/Sources/VCamReceiver.h
+++ VCamJoy/Sources/VCamReceiver.h
@@ -1,13 +1,10 @@
 #import <Foundation/Foundation.h>
-#import <CoreMedia/CoreMedia.h>
 #import <UIKit/UIKit.h>
 
 @interface VCamReceiver : NSObject <NSURLSessionDataDelegate>
 + (instancetype)sharedReceiver;
 - (void)startWithURL:(NSURL *)url;
 - (void)stop;
-- (CMSampleBufferRef)copyLatestSampleBuffer CF_RETURNS_RETAINED;
 @property (nonatomic, readonly) BOOL isConnected;
 @property (nonatomic, readonly) UIImage *latestImage;
-@property (nonatomic, readonly) double currentFPS;
 @end
